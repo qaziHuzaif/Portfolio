4d680be584fd90e41f186369db5fc0b3115e029d
Musaib Shabir <bnny633@gmail.com>
2025-03-29 04:18:18 +0530

    adjusted SubHeadLine Color for Light Theme

--- site/src/jsMain/kotlin/musaib/SiteTheme.kt
+++ site/src/jsMain/kotlin/musaib/SiteTheme.kt
@@ -16,41 +16,29 @@ class SitePalette(
     val nearBackground: Color,
     val cobweb: Color,
     val subHeadLine: Color,
-    val mainText: Color,
     val buttonBackground: Color,
     val buttonText: Color,
-    val brand: Brand,
-) {
-    class Brand(
-        val primary: Color = Color.rgb(0x3C83EF),
-        val accent: Color = Color.rgb(0xF3DB5B),
-    )
-}
+
+)
 
 object SitePalettes {
     val light = SitePalette(
         nearBackground = Color.rgb(0xF4F6FA),
         cobweb = Colors.LightGray,
-        mainText = Colors.White,
-        subHeadLine = Color.rgb(0x228B22),
+
+        subHeadLine = Color.rgb(0x0da30d),
         buttonBackground = Colors.Black,
         buttonText = Colors.White,
-        brand = SitePalette.Brand(
-            primary = Color.rgb(0x3C83EF),
-            accent = Color.rgb(0xFCBA03),
-        )
+
     )
     val dark = SitePalette(
         nearBackground = Color.rgb(0x454545),
         cobweb = Colors.LightGray.inverted(),
-        mainText = Colors.White,
+
         subHeadLine = Color.rgb(0x49D344),
         buttonBackground = Colors.White,
         buttonText = Colors.Black,
-        brand = SitePalette.Brand(
-            primary = Color.rgb(0x3C83EF),
-            accent = Color.rgb(0xF3DB5B),
-        )
+
     )
 }
 
